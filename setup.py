2f55d4334f2e4e6edcdce280e064dc7b03116de5
scrat-online <scrat_online@gmx.de>
2017-05-11 15:19:06 +0200

    set setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+Project: 
+File: setup
+
+Created by Scrat on 11.05.2017
+"""
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pySTARMA',
+    version='0.1.0',
+    description='',
+    long_description=readme,
+    author='Andreas Wolf',
+    author_email='andreas.wolf.ke@gmail.com',
+    url='https://github.com/scrat-online/pySTARMA.git',
+    license=license,
+    packages=find_packages()
+)
